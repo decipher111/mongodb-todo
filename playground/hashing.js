efee1a97cebbd1c77bd83644b6d6898487eeab5f
Raghav Khanna <raghav@Raghavs-MacBook-Air.local>
2018-12-28 14:47:59 +0530

    setting up private route

---
+++ playground/hashing.js
@@ -0,0 +1,23 @@
+const{SHA256} = require('crypto-js');
+
+var x = 'I am user number 3'
+console.log(SHA256(x).toString())
+
+var data = {
+    id: 4
+}
+
+var token = {
+    data,
+    hash: SHA256(JSON.stringify(data)+"salt").toString()
+}
+
+
+var resulthash = SHA256(JSON.stringify(token.data)+"salt").toString()
+
+if(token.hash === resulthash){
+    console.log('data not manipulated')
+}
+else{
+    console.log('data was manipulated')
+}
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {mongoose} = require('./db/mongoose.js')
 const {User} = require('./models/user.js')
 const {Todo} = require('./models/todo.js')
 const {ObjectID} = require('mongodb')
+const {authenticate} = require('./middlewares/authenticate')
 
 var app = express();
 const port = process.env.PORT || 3000
@@ -92,16 +93,21 @@ app.patch('/todos/:id', (req, res) => {
       })
 })
 
-app.post('/users', (req,res) => {
-    var body = _.pick(req.body,['email', 'password'])
-
-    var user = new User(body)
-    
-    user.save().then((data) => {
-        res.send(data)
-    },(err) => {
-        res.status(400).send(err)
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+  
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      res.header('x-auth', token).send(user);
+    }).catch((e) => {
+      res.status(400).send(e);
     })
+  });
+
+app.get('/users/me', authenticate, (req,res) => {
+    res.send(req.user)
 })
 
  
