d25717f6f554911cabf9e406c3fe3dfaa6411d83
Raghav Khanna <raghav@Raghavs-MacBook-Air.local>
2019-01-05 18:28:39 +0530

    hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,23 +1,40 @@
 const{SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs')
 
-var x = 'I am user number 3'
-console.log(SHA256(x).toString())
+var password = 'abc123'
 
-var data = {
-    id: 4
-}
+bcrypt.genSalt(10, (err,salt) => {
+    bcrypt.hash(password, salt, (err,hash) => {
+        console.log(hash)
+    })
+})
 
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data)+"salt").toString()
-}
+var hashedPassword = '$2a$10$ztrxfsK0IKhl/cTtQq3IO.zgXQdrZ9I4v8Ql3bus7e8mE47Dtubg2';
+
+bcrypt.compare('abc123', hashedPassword, (err,res) => {
+    console.log(res)
+})
 
 
-var resulthash = SHA256(JSON.stringify(token.data)+"salt").toString()
 
-if(token.hash === resulthash){
-    console.log('data not manipulated')
-}
-else{
-    console.log('data was manipulated')
-}
+// var x = 'I am user number 3'
+// console.log(SHA256(x).toString())
+
+// var data = {
+//     id: 4
+// }
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data)+"salt").toString()
+// }
+
+
+// var resulthash = SHA256(JSON.stringify(token.data)+"salt").toString()
+
+// if(token.hash === resulthash){
+//     console.log('data not manipulated')
+// }
+// else{
+//     console.log('data was manipulated')
+// }
